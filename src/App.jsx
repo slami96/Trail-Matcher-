e27475f596567a0269446913eadd34ad5efd6d89
slami96 <adam.slamen@gmail.com>
2025-10-29 17:58:14 +0100

    Implement splash screen with session storage
    Added splash screen functionality to the app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar.jsx'
+import SplashScreen from './components/SplashScreen.jsx'
+import BottomNav from './components/BottomNav.jsx'
 import Welcome from './pages/Welcome.jsx'
 import Quiz from './pages/Quiz.jsx'
 import Results from './pages/Results.jsx'
@@ -7,9 +9,27 @@ import TrailDetail from './pages/TrailDetail.jsx'
 import MyPlan from './pages/MyPlan.jsx'
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true)
+
+  // Check if splash has been shown in this session
+  useEffect(() => {
+    const splashShown = sessionStorage.getItem('splashShown')
+    if (splashShown) {
+      setShowSplash(false)
+    }
+  }, [])
+
+  const handleSplashComplete = () => {
+    setShowSplash(false)
+    sessionStorage.setItem('splashShown', 'true')
+  }
+
+  if (showSplash) {
+    return <SplashScreen onComplete={handleSplashComplete} />
+  }
+
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/quiz" element={<Quiz />} />
@@ -17,6 +37,7 @@ function App() {
         <Route path="/trail/:id" element={<TrailDetail />} />
         <Route path="/my-plan" element={<MyPlan />} />
       </Routes>
+      <BottomNav />
     </Router>
   )
 }
