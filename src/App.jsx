fefc5e6850a899760bb49f82af59f86f330e4b09
slami96 <adam.slamen@gmail.com>
2025-10-31 17:04:07 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import SplashScreen from './components/SplashScreen.jsx'
 import BottomNav from './components/BottomNav.jsx'
 import Welcome from './pages/Welcome.jsx'
@@ -7,6 +7,24 @@ import Quiz from './pages/Quiz.jsx'
 import Results from './pages/Results.jsx'
 import TrailDetail from './pages/TrailDetail.jsx'
 import MyPlan from './pages/MyPlan.jsx'
+
+function AppContent() {
+  const location = useLocation()
+  const hideNav = location.pathname === '/quiz'
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/trail/:id" element={<TrailDetail />} />
+        <Route path="/my-plan" element={<MyPlan />} />
+      </Routes>
+      {!hideNav && <BottomNav />}
+    </>
+  )
+}
 
 function App() {
   const [showSplash, setShowSplash] = useState(true)
@@ -30,14 +48,7 @@ function App() {
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/trail/:id" element={<TrailDetail />} />
-        <Route path="/my-plan" element={<MyPlan />} />
-      </Routes>
-      <BottomNav />
+      <AppContent />
     </Router>
   )
 }
