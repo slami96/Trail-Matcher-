c83ec62ee4147bf2763cd4e4b1adb39cbe89e5ab
slami96 <adam.slamen@gmail.com>
2025-11-03 21:43:37 +0100

    Update supabaseClient.js

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// grab the environment variables from Vercel
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-// create the supabase client for database operations
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
